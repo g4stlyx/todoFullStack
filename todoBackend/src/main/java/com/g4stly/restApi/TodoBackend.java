dbf3c88c7419ba933641485e4031865d9ec8713d
g4stlyx <g4stlyonline@gmail.com>
2024-07-16 21:50:55 +0300

    database connection with aws and mysql

--- todoBackend/src/main/java/com/g4stly/restApi/TodoBackend.java
+++ todoBackend/src/main/java/com/g4stly/restApi/TodoBackend.java
@@ -2,10 +2,14 @@ package com.g4stly.restApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableJpaRepositories // added for mysql connection
+@EntityScan // added for mysql connection
 @SpringBootApplication
 public class TodoBackend {
 
--- todoBackend/src/main/resources/data.sql
+++ todoBackend/src/main/resources/data.sql
@@ -9,3 +9,30 @@ values (10003, 'Learn DevOps', false, CURRENT_DATE(), 'g4stly');
 
 insert into todos (id, description, done, target_date,username)
 values (10004, 'Learn qwewqeqw', false, CURRENT_DATE(), 'user2');
+
+/* connecting your db to the app
+    in application.properties, add these(ofc edit the required parts first):
+
+    spring.datasource.url=jdbc:mysql://<your-rds-endpoint>:3306/<your-database-name>
+    spring.datasource.username=<your-username>
+    spring.datasource.password=<your-password>
+    spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+    spring.jpa.hibernate.ddl-auto=update
+    spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dialect
+
+    create application.yml and add these(ofc edit the required parts first):
+
+    spring:
+        datasource:
+            url: <url>
+            username: <username>
+            password: <pwd>
+            driver-class-name: com.mysql.cj.jdbc.Driver
+        jpa:
+            hibernate:
+                ddl-auto: update
+            properties:
+            hibernate:
+                dialect: org.hibernate.dialect.MySQL8Dialect
+
+*/
